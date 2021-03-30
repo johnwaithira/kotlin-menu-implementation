da0a8077ea40fbf7a7ab0f32aa6b0664270746dc
jonwaithira <78647762+codewith-jon@users.noreply.github.com>
2021-03-30 12:43:36 +0300

    Initial commit

---
+++ app/src/main/java/com/waithira/menu/MainActivity.kt
@@ -0,0 +1,14 @@
+sdkpackage com.waithira.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
